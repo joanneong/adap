4509783238fe198b5a50d09f1ad789d306dc6ae9
ying95 <vpsy28@gmail.com>
2018-11-07 16:53:05 +0800

    Update adap/htdocs/search.php

---
+++ adap/htdocs/search.php
@@ -0,0 +1,39 @@
+<?php
+  include 'homepage/navbar_before_login.php';
+?>
+
+<!DOCTYPE html>  
+<html lang="en">
+<head>
+  <title>ADAP</title>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="../htdocs/css/materialize.min.css"  media="screen,projection"/>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <style>li {list-style: none;}</style>
+  <style></style>
+</head>
+<body>
+<STYLE>
+   .section {
+	text-align: center;
+  font-size: 200%;
+	}
+
+	img {
+	width: 40%;
+	margin: auto;
+     vertical-align: middle;
+	}
+
+ </STYLE>
+  <div class="section" id="search" >
+	<H1><strong>Paste your results here:</strong></H1>
+<br><br>
+</div>
+
+</body>
+</html>
