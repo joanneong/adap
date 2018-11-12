ab5c75197870a03d93b7a1bf8c2630ea4cb889eb
ying95 <vpsy28@gmail.com>
2018-11-12 18:47:11 +0800

    Update adap/htdocs/php/check_mac.php
    - whitespaces beginning and the end of the mac address accounted for
    - tidied up code but still left with some echo statements if needed (commented out)

--- adap/htdocs/php/check_mac.php
+++ adap/htdocs/php/check_mac.php
@@ -2,20 +2,15 @@
   // Connect to the database
   include("../config.php");
 
-//  echo "testing12344588 ";
-  $query  = "SELECT * FROM router WHERE mac_address = '" . $_POST[mac_address] . "'";
-
+  $trimmed = trim($_POST[mac_address]);
+//  echo $trimmed;
+  $query  = "SELECT * FROM router WHERE mac_address = '$trimmed'";
 //  echo $query;
-
   $result = pg_query($db, $query);
-
 //  echo $result;
-
   $row = pg_fetch_row($result);
-
 //  echo $row[1];
-
-  if ($row[1] == $_POST[mac_address]) {
+  if ($row[1] == $trimmed) {
     echo "SUCCESS!";
   }
 
